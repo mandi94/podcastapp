2f86ed56223a3e214e22be5fa0c3d1bab20b8d3a
Amanda Camacho <ajmandi94@gmail.com>
2023-06-19 00:26:01 -0500

    V1.1: Update the error page

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>Oops! The page you are looking for does not exist.</p>
+    <div className="container text-center">
+      <h1 className="display-1">404</h1>
+      <p className="lead">
+        Oops! The page you're looking for could not be found.
+      </p>
+      <a href="/" className="btn btn-primary mt-3">
+        Go back to the homepage
+      </a>
     </div>
   );
-}
+};
 
 export default NotFound;
