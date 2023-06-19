7cbb55dcd73d3ddbda4cba82e6704300349f7443
Amanda Camacho <ajmandi94@gmail.com>
2023-06-19 00:55:11 -0500

    V1.1: Loading component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import NotFound from "./views/NotFound";
@@ -7,10 +7,31 @@ import PodcastEpisodeDetailsView from "./views/PodcastEpisodeDetailsView";
 import PodcastView from "./views/PodcastView";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsLoading(true);
+
+      // Simulate a delay for demonstration purposes
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    };
+
+    // Add the event listener for route change
+    window.addEventListener("routeChange", handleRouteChange);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("routeChange", handleRouteChange);
+    };
+  }, []);
+
   return (
     <Router>
       <div className="app">
-        <Header />
+        <Header isLoading={isLoading} />
         <Routes>
           <Route exact path="/" element={<PodcastView />} />
           <Route path="/podcast/:podcastId" element={<PodcastDetailsView />} />
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Header() {
-
-  
+function Header({ isLoading }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
       <div className="container">
-        <Link className="navbar-brand text-primary" to="/">Podcaster</Link>
-        {/* <div className="ml-auto">
-          <div className="spinner-border text-primary" role="status">
-          </div>
-        </div> */}
+        <Link className="navbar-brand text-primary" to="/">
+          Podcaster
+        </Link>
+        <div className="ml-auto">
+          {isLoading && (
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
